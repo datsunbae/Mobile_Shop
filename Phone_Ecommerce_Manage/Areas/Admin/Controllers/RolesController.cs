4c63cd2296a302505fb88a4ee5892dac7ed3d694
Nguyen Van Dat <datsunbae@gmail.com>
2022-10-28 22:54:35 +0700

    CURD Role

---
+++ Phone_Ecommerce_Manage/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Phone_Ecommerce_Manage.Models;
+
+namespace Phone_Ecommerce_Manage.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly MobileShop_DBContext _context;
+
+        public RolesController(MobileShop_DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminRoles
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Roles.ToListAsync());
+        }
+
+        // GET: Admin/AdminRoles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Roles
+                .FirstOrDefaultAsync(m => m.IdRole == id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // GET: Admin/AdminRoles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminRoles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdRole,RoleName")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(role);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(role);
+        }
+
+        // GET: Admin/AdminRoles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Roles.FindAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Admin/AdminRoles/Edit/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdRole,RoleName")] Role role)
+        {
+
+            if (id != role.IdRole)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(role);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoleExists(role.IdRole))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(role);
+        }
+
+        // GET: Admin/AdminRoles/Delete/id
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Roles
+                .FirstOrDefaultAsync(m => m.IdRole == id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // POST: Admin/AdminRoles/Delete/id
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Roles == null)
+            {
+                return Problem("Entity set 'MobileShop_DBContext.Roles'  is null.");
+            }
+            var role = await _context.Roles.FindAsync(id);
+            if (role != null)
+            {
+                _context.Roles.Remove(role);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Roles");
+        }
+
+        private bool RoleExists(int id)
+        {
+          return _context.Roles.Any(e => e.IdRole == id);
+        }
+    }
+}
