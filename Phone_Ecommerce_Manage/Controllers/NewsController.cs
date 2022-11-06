edf28e92e3cebe840add284864834d679da0ea6b
trancaodai1651 <trancaodai1651@gmail.com>
2022-11-06 20:27:13 +0700

    Create: News, UploadFile

---
+++ Phone_Ecommerce_Manage/Controllers/NewsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Phone_Ecommerce_Manage.Models;
+
+namespace Phone_Ecommerce_Manage.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly MobileShop_DBContext _context;
+
+        public NewsController(MobileShop_DBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.ListCategoryNews = await _context.CategoryNews.ToListAsync();
+            return View(await _context.News.ToListAsync());
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Phone_Ecommerce_Manage/Utilities/UploadFile.cs
@@ -0,0 +1,40 @@
+﻿namespace Phone_Ecommerce_Manage.Utilities
+{
+    public static class UploadFile
+    {
+        public static void CreateIfMissing(string path)
+        {
+            bool folderExists = Directory.Exists(path);
+            if (!folderExists)
+                Directory.CreateDirectory(path);
+        }
+        public static async Task<string> UploadImage(Microsoft.AspNetCore.Http.IFormFile file, string sDirectory, string newname = null)
+        {
+            try
+            {
+                if (newname == null) newname = file.FileName;
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", sDirectory);
+                CreateIfMissing(path);
+                string pathFile = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", sDirectory, newname);
+                var supportedTypes = new[] { "jpg", "jpeg", "png", "gif" };
+                var fileExt = System.IO.Path.GetExtension(file.FileName).Substring(1);
+                if (!supportedTypes.Contains(fileExt.ToLower())) /// Khác các file định nghĩa
+                {
+                    return null;
+                }
+                else
+                {
+                    using (var stream = new FileStream(pathFile, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    return newname;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
