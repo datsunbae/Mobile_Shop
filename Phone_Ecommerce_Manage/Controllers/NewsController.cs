00610f28c72a5fd519f5403555f373f46359f910
trancaodai1651 <trancaodai1651@gmail.com>
2022-11-07 23:14:35 +0700

    Update News at User

--- Phone_Ecommerce_Manage/Controllers/NewsController.cs
+++ Phone_Ecommerce_Manage/Controllers/NewsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Phone_Ecommerce_Manage.Models;
+using Phone_Ecommerce_Manage.ModelViews;
 
 namespace Phone_Ecommerce_Manage.Controllers
 {
@@ -14,12 +16,33 @@ namespace Phone_Ecommerce_Manage.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            ViewBag.ListCategoryNews = await _context.CategoryNews.ToListAsync();
-            return View(await _context.News.ToListAsync());
+            HomeProductViewModel model = new HomeProductViewModel();
+            var news = _context.News
+                    .AsNoTracking()
+                    .Where(x => x.IsHot == true)
+                    .OrderByDescending(x => x.CreateDate)
+                    .Take(1)
+                    .ToList();
+            model.listNews = news;
+            return View(model);
         }
-        public IActionResult Detail()
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
+            if (id == null || _context.News == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News
+                .Include(n => n.IdAccountUserNavigation)
+                .Include(n => n.IdCategoryNewsNavigation)
+                .FirstOrDefaultAsync(m => m.IdNews == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
         }
 
     }
