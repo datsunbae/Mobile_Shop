0e9bd65a4963461ea56f3ee33b6ed843ae0acf68
trancaodai <trancaodai1651@gmail.com>
2022-11-15 14:40:48 +0700

    Create: Voucher User

---
+++ Phone_Ecommerce_Manage/Controllers/VoucherController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Phone_Ecommerce_Manage.Models;
+
+namespace Phone_Ecommerce_Manage.Controllers
+{
+    public class VoucherController : Controller
+    {
+        private readonly MobileShop_DBContext _context;
+
+        public VoucherController(MobileShop_DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Voucher
+        public async Task<IActionResult> Index()
+        {
+            List<Voucher> vouchers = await _context.Vouchers.ToListAsync();
+            return View(vouchers);
+        }
+    }
+}
